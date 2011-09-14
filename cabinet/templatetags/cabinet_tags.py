f75fee10c9cf679d4165f1f2108a8833aba172e6
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-09-14 19:25:23 +0400

    Bug fix

--- cabinet/templatetags/cabinet_tags.py
+++ cabinet/templatetags/cabinet_tags.py
@@ -4,3 +4,13 @@ register = template.Library()
 @register.filter
 def cut_http(value):
     return value[7:-1]
+
+@register.inclusion_tag("tags/greeting.html")
+def greeting(request):
+    try:
+        is_auth = request.user.first_name
+    except :
+        is_auth = False
+    return {
+        'is_auth': is_auth,
+}
