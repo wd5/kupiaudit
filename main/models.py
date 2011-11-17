8f1df132edb47216df8249a81fc844d33a24971b
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-11-17 18:40:58 +0400

    Bug fix

--- main/models.py
+++ main/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from filebrowser.fields import FileBrowseField
 
 class Pocket(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     image = models.ImageField(upload_to="pockets_image")
+    image = FileBrowseField("Image", max_length=200, directory="", extensions=[".jpg", ".png"], blank=True, null=True)
     mini_description = models.TextField()
     description = models.TextField()
 
